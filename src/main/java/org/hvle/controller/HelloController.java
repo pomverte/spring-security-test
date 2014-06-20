15b23583256fbf6dbc988b6faebdd450b3510877
hvle <ha.hong.viet.le@cgi.com>
2014-06-21 01:28:20 +0200

    externalisation des libelles dans un fichier properties et appel à ceux ci via @Value

--- src/main/java/org/hvle/controller/HelloController.java
+++ src/main/java/org/hvle/controller/HelloController.java
@@ -1,5 +1,6 @@
 package org.hvle.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,16 +8,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
- 
+
+	@Value("${title}")
+	private String title;
+
+	@Value("${message}")
+	private String message;
+
 	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
 	public ModelAndView welcomePage() {
- 
+
 		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is welcome page!");
+		model.addObject("title", this.title);
+		model.addObject("message", this.message);
 		model.setViewName("hello");
 		return model;
- 
+
 	}
 
 }
